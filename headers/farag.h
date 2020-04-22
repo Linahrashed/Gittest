65f4358804c36ec38ab0bb733888724c5c74660f
Linah <Linahrashed@aucegypt.edu>
2020-04-22 16:11:36 -0400

    mesage

--- headers/farag.h
+++ headers/farag.h
@@ -1,13 +1,8 @@
 #include <iostream>
 using namespace std;
 
-<<<<<<< HEAD
-<<<<<<< HEAD
-void farag();
 
-=======
 void farag();
 >>>>>>> origin/mfaraggg
-=======
-void farag();
->>>>>>> origin/MuhammedElemam
+
+
